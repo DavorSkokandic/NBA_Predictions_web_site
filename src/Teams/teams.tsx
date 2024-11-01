d7a903afb70eac20a56b5829532b6d577d53ff4e
Davor Skokandić <davor.skokandic46@gmail.com>
2024-11-01 14:51:06 +0100

    Frist commit

--- src/Teams/teams.tsx
+++ src/Teams/teams.tsx
@@ -4,13 +4,13 @@ import './teams.css';
 function Teams(){
     return(
         <div className="container">
-            <div className="team">
+            <div className="team-DAL">
                 <span>Dallas</span>
-                <img className="team-logo" src="./images/dal.png"></img>
+                <img className="team-logo" src="./dal.png" alt=""></img>
             </div>
-            <div className="team">
+            <div className="team-LAL">
                 <span>Los Angeles Lakers</span>
-                <img className="team-logo" src="./images/dal.png"></img>
+                <img className="team-logo" src="./images/dal.png" alt=""></img>
             </div>
         </div>
     );
